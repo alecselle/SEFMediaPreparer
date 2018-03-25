74a456e3600f0043d075372bded745b7054e2a84
SEF Jenkins <jenkins@superepicfuntime.com>
2018-03-24 21:11:27 -0500

    Automatic Commit (Jenkins)

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -3,12 +3,14 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/container/vector.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 #include <nlohmann/json.hpp>
 #include <string>
 
 namespace bf = boost::filesystem;
 namespace bc = boost::container;
 namespace ba = boost::algorithm;
+namespace bpo = boost::program_options;
 using namespace nlohmann;
 using namespace std;
 
