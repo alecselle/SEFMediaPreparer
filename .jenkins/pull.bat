f62a72fdcd1225b78340e4b35376551c3a298e0e
SEF Jenkins <jenkins@superepicfuntime.com>
2018-04-04 12:33:24 -0500

    Automatic Commit (Jenkins)

--- .jenkins/pull.bat
+++ .jenkins/pull.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
-echo.Pulling to local workspace...
+echo.Pulling to local workspace from git...
 cd /d "F:\Workspace\cpp\SEFMediaPreparer"
 call git pull
 echo.
--- .jenkins/push.bat
+++ .jenkins/push.bat
@@ -5,6 +5,5 @@ cd /d "F:\Workspace\cpp\SEFMediaPreparer"
 call git add *
 call git commit -m "Automatic Commit (Jenkins)"
 call git push
-echo.Pulling git changes to build directory...
 echo.
 endlocal
